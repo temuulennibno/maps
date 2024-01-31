7726340fc32283fd0821c06c7db3de6282faf783
Temuulen Narmandakh <temuulen.n@pinecone.mn>
2024-01-31 19:14:29 +0800

    example

--- app/(tabs)/two.tsx
+++ app/(tabs)/two.tsx
@@ -1,18 +1,50 @@
 import { Button, StyleSheet, Text, View } from "react-native";
 import * as Location from "expo-location";
 import { useEffect, useState } from "react";
-import MapView, { Marker } from "react-native-maps";
+import MapView, { Circle, Marker } from "react-native-maps";
+import axios from "axios";
 
 export default function TabTwoScreen() {
   const [status, requestPermission] = Location.useForegroundPermissions();
   const [location, setLocation] = useState<any>(null);
-  // const [status, requestPermission] = Location.useBackgroundPermissions();
+  const [marker, setMarker] = useState<any>(null);
+  const [places, setPlaces] = useState<any[]>([]);
+
   useEffect(() => {
     Location.getCurrentPositionAsync({}).then((location) => {
       console.log("location", location);
       setLocation(location);
+      setMarker({ longitude: location.coords.longitude, latitude: location.coords.latitude });
     });
   }, []);
+
+  useEffect(() => {
+    if (marker) {
+      (async () => {
+        const options = {
+          method: "GET",
+          url: "https://opentripmap-places-v1.p.rapidapi.com/en/places/radius",
+          params: {
+            radius: "500",
+            lon: marker.longitude,
+            lat: marker.latitude,
+            kinds: "interesting_places",
+          },
+          headers: {
+            "X-RapidAPI-Key": "fda9945fc6msh3d38c07c56e6013p18429ajsn21c639670159",
+            "X-RapidAPI-Host": "opentripmap-places-v1.p.rapidapi.com",
+          },
+        };
+
+        try {
+          const response = await axios.request(options);
+          setPlaces(response.data.features);
+        } catch (error) {
+          console.error(error);
+        }
+      })();
+    }
+  }, [marker]);
 
   if (!status || !status.granted) {
     return (
@@ -30,20 +62,33 @@ export default function TabTwoScreen() {
           initialRegion={{
             latitude: location.coords.latitude,
             longitude: location.coords.longitude,
-            latitudeDelta: 0.0922,
-            longitudeDelta: 0.0421,
+            latitudeDelta: 1,
+            longitudeDelta: 1,
           }}
           zoomEnabled
+          onPress={(e) => {
+            setMarker(e.nativeEvent.coordinate);
+          }}
         >
+          <Circle center={marker} radius={500} fillColor={"rgba(24, 110, 247,0.1)"} strokeColor="rgb(24, 110, 247)" />
           <Marker
-            coordinate={{
-              latitude: location.coords.latitude,
-              longitude: location.coords.longitude,
+            coordinate={marker}
+            draggable
+            image={require("../../assets/pin.png")}
+            onDragEnd={(e) => {
+              setMarker(e.nativeEvent.coordinate);
             }}
-            image={{ uri: "https://tmpfiles.nohat.cc/full-m2H7K9i8Z5K9Z5d3.png" }}
-            title={"Me"}
-            description={"My position"}
           />
+          {places.map((place) => (
+            <Marker
+              key={place.properties.xid}
+              coordinate={{
+                latitude: place.geometry.coordinates[1],
+                longitude: place.geometry.coordinates[0],
+              }}
+              title={place.properties.name}
+            />
+          ))}
         </MapView>
       )}
     </View>
